10eda809ea2f05a6536662e1596e252da227fcd7
ErzaQorri <93944366+ErzaQorrii@users.noreply.github.com>
2024-05-10 11:03:19 +0200

    Fixed incorrect route path for user  sign up

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,26 @@
+// import React from 'react';
+// import './App.css';
+// import LoginSignup from './Components/LoginSignup/LoginSignup';
+// import Test_user from './Components/NormalUser/Test_user';
+// import Test_admin from './Components/Admin/Test_admin';
+// import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//     <Routes>
+//       <Route path="/" element={<Navigate replace to="/login" />} />  
+//       <Route path="/login" element={<LoginSignup action="Log In" />} /> 
+//       <Route path="/register" element={<LoginSignup action =" Sign Up" />} /> 
+//       <Route path="/user" element={<Test_user />} />                
+//       <Route path="/admin" element={<Test_admin />} />              
+//     </Routes>
+//   </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
 import React from 'react';
 import './App.css';
 import LoginSignup from './Components/LoginSignup/LoginSignup';
@@ -10,7 +33,8 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Navigate replace to="/login" />} />  
-      <Route path="/login" element={<LoginSignup />} />             
+      <Route path="/login" element={<LoginSignup action="Login" />} />       
+      <Route path="/register" element={<LoginSignup action="Sign Up" />} />             
       <Route path="/user" element={<Test_user />} />                
       <Route path="/admin" element={<Test_admin />} />              
     </Routes>
